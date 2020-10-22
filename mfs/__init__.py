95fb619aa5150bf0ab3d0e87480396e146eae837
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2020-10-22 15:57:17 +0200

    bumpversion

--- mfs/__init__.py
+++ mfs/__init__.py
@@ -4,6 +4,6 @@ from . import examples
 
 from .helpers import plot_vector_B_field
 
-__version__ = "1.0.0"
+__version__ = "__version__ = '1.1.0'"
 
 __all__ = ("helpers", "sources", "examples", "plot_vector_B_field")
