a68d34b89c4e49f5f9760ce97d2df6fd12c63bec
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2020-10-23 14:01:54 +0200

    Bumpversion
    Increment patch version after fixing `examples`

--- docs/conf.py
+++ docs/conf.py
@@ -27,9 +27,9 @@ copyright = '2019, Simon Ball'
 author = 'Simon Ball'
 
 # The short X.Y version
-version = '1.1.0'
+version = '1.1.1'
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.1.1'
 
 
 # -- General configuration ---------------------------------------------------
--- mfs/__init__.py
+++ mfs/__init__.py
@@ -4,6 +4,6 @@ from . import examples
 
 from .helpers import plot_vector_B_field
 
-__version__ = '1.1.0'
+__version__ = '1.1.1'
 
 __all__ = ("helpers", "sources", "examples", "plot_vector_B_field")
