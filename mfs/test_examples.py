c59396902126eb51f4854e5bbfe0a02458f1a91b
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2020-10-23 13:46:00 +0200

    Add test_examples.py

---
+++ mfs/test_examples.py
@@ -0,0 +1,18 @@
+from . import examples
+
+
+def test_exmaple_permanent_magnets():
+    examples.DEBUG = True
+    examples.permanent_magnets()
+    examples.plt.close("all")
+    
+def test_example_rectangular_coil_pair():
+    examples.DEBUG = True
+    examples.rectangular_coil_pair()
+    examples.plt.close("all")
+
+def test_example_circular_coil_pair():
+    examples.DEBUG = True
+    examples.circular_coil_pair()
+    examples.plt.close("all")
+    
