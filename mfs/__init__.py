630b1bd9969d43f86534a39d5f46eb1f5cfe1361
Simon Ball <48679510+simon-ball@users.noreply.github.com>
2020-11-01 18:43:25 +0100

    Make __version__ importable

--- mfs/__init__.py
+++ mfs/__init__.py
@@ -1,9 +1,10 @@
+from .version import __version__
+
 from . import helpers
 from . import sources
 from . import examples
 
 from .helpers import plot_vector_B_field
 
-__version__ = "1.2.0"
 
 __all__ = ("helpers", "sources", "examples", "plot_vector_B_field")
